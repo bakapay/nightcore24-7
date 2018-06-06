ab0a5da99aebd7a88d67b4d3d44cff0321d65b17
zapayyyy <36130036+zapayyyy@users.noreply.github.com>
2018-06-06 14:52:35 +0300

    Update index.js

--- index.js
+++ index.js
@@ -2,18 +2,13 @@ const Discord = require("discord.js")
 const ytdl = require("ytdl-core")
 const client = new Discord.Client();
 let voiceChannel = client.channels.get("452486931255918613")
-let connection = voiceChannel.join();
-const dispatcher = connection.playStream(ytdl('https://www.youtube.com/watch?v=ZlAU_w7-Xp8', { filter: 'audioonly' }));
-function onEnd() {
-connection.playStream(ytdl('https://www.youtube.com/watch?v=ZlAU_w7-Xp8', { filter: 'audioonly' }));
-}
 
 client.on("ready", ready => {
 	console.log("ready")
 });
 
   client.on(`ready`, member => {
-        client.user.setActivity(`SOME NIGHTCORE`, {
+        client.user.setActivity(`SOME GOOD MUSIC`, {
         type: "Listening"
     });
 });
@@ -23,12 +18,17 @@ client.on("ready", ready => {
 });
 
 client.on("ready", ready => {
-connection.playStream(ytdl('https://www.youtube.com/watch?v=ZlAU_w7-Xp8', { filter: 'audioonly' }));
-});
+voiceChannel.connect().then (connection => {
+const dispatcher = connection.playStream(ytdl('https://www.youtube.com/watch?v=ZlAU_w7-Xp8', { filter: 'audioonly' }));
 
+function onEnd() {
+connection.playStream(ytdl('https://www.youtube.com/watch?v=ZlAU_w7-Xp8', { filter: 'audioonly' }));
+}
 
 dispatcher.on("end", () => {
 onEnd();
+});
+})
 });
 
 
