46245941301e41e86b4e7f5d8575122fa07bbe17
zapayyyy <36130036+zapayyyy@users.noreply.github.com>
2018-06-06 14:45:38 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,17 +1,19 @@
 const Discord = require("discord.js")
 const ytdl = require("ytdl-core")
 const client = new Discord.Client();
+let voiceChannel = client.channels.get("452486931255918613")
+let connection = voiceChannel.join();
 const dispatcher = connection.playStream(ytdl('https://www.youtube.com/watch?v=ZlAU_w7-Xp8', { filter: 'audioonly' }));
 function onEnd() {
-connection.playStream(ytdl('https://www.youtube.com/watch?v=pGzK01Wu8zo', { filter: 'audioonly' }));
-};
+connection.playStream(ytdl('https://www.youtube.com/watch?v=ZlAU_w7-Xp8', { filter: 'audioonly' }));
+}
 
 client.on("ready", ready => {
 	console.log("ready")
 });
 
   client.on(`ready`, member => {
-        client.user.setActivity(`SOME NIGHTCORE`, {
+        client.user.setActivity(`SOME GOOD MUSIC`, {
         type: "Listening"
     });
 });
@@ -19,15 +21,16 @@ client.on("ready", ready => {
 client.on("ready", () => {
   client.user.setStatus("dnd")
 });
-
-client.on("ready", () => {
-client.channels.get("452486931255918613").join().then (connection => {
-connection.playStream(ytdl('https://www.youtube.com/watch?v=ZlAU_w7-Xp8', { filter: 'audioonly' }))
+client.on("ready", () {
+voiceChannel.join().then (connection => {
+connection.playStream(ytdl('https://www.youtube.com/watch?v=ZlAU_w7-Xp8', { filter: 'audioonly' }));
 })
 });
+
 
 dispatcher.on("end", () => {
 onEnd();
 });
+
 
 client.login(process.env.BOT_TOKEN)
